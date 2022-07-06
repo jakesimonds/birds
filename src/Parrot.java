8d7d478609e5ac1cf862d757c07f8c2d5809c427
Nick Barnes <82965612+bicknarnes@users.noreply.github.com>
2022-07-06 11:31:08 -0400

    Update Parrot.java

--- src/Parrot.java
+++ src/Parrot.java
@@ -1,9 +1,35 @@
 public class Parrot extends birdClass {
-    public Parrot(String name, int wingNum) {
+    
+    // Member fields: SHOULD THESE BE INCLUDED HERE IF THEY ARE INHERITED FROM BIRD CLASS?
+    String name;
+    int wingNum;
+    boolean extinct;
+    String description;
+    
+    // Parrot-specific attributes:
+    int knownWords;
+    String favoritePhrase;
+    
+   
+    // Constructor:
+    public Parrot(String name, int wingNum, int knownWords, String favoritePhrase) {
         super(name, wingNum);
-
+        
+        this.description = "Parrots have a short, curved beak and are known for their intelligence and ability to mimic sounds.";
+        this.knownWords = knownWords;
+        this.favoritePhrase = favoritePhrase;
     }
-    //parrot child class
+    
+    // Get Known Words
+    public int getKnownWords() {
+        return this.knownWords;   
+    }
+    
+    // Get Favorite Phrase
+    public String getFavoritePhrase() {
+        return this.favoritePhrase;
+    }
+    
 
 
 }
