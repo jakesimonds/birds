fc3b52657efbf3505d8ca7e1cde0454e503404b1
Nick Barnes <82965612+bicknarnes@users.noreply.github.com>
2022-07-06 11:36:27 -0400

    Create WaterBird.java

---
+++ src/WaterBird.java
@@ -0,0 +1,16 @@
+public class WaterBird extends Bird {
+  
+  // Member fields:
+  String bodyOfWater;
+  
+  // Methods:
+  public String getBodyOfWater() {
+    return this.bodyOfWater;
+  }
+  
+  
+  
+  
+  
+  
+}
