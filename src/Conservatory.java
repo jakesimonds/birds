8d2237000948a98f82a358fb3405a9b94098ab4d
Nick Barnes <82965612+bicknarnes@users.noreply.github.com>
2022-07-06 11:13:16 -0400

    Update Conservatory.java

--- src/Conservatory.java
+++ src/Conservatory.java
@@ -1,2 +1,38 @@
+// Represents a conservatory which can house up to 20 Aviareis
+// which house rescued birds
+
 public interface Conservatory {
+  
+  // Constructor:
+  public Conservatory();
+  
+  // Guest Lookup
+  public String guestLookup(Bird bird);
+  
+  // Print Index
+  public void printIndex();
+  
+  // Print Map
+  public void printMap();
+  
+  // Calculate Food
+  public String calculateFood();
+  
+  // Rescue New Bird
+  // FAILS if conservatory is full
+  public void rescueBird();
+  
+  // (Private) Add Aviary -- Adds a new Aviary to the Conservatory
+  // FAILS if there are already 20 aviaries
+  private void addAviary();
+  
+  // (Private) Get Aviary List -- 
+  // TODO : WHAT TYPE DOES THIS RETURN (Aviary Array? Aviary[] ?)
+  private getAviaryList();
+  
+  // Is Full -- Returns true if the conservatory already has 20 Aviaries, false otherwise
+  // TODO: SHOULD THIS CHECK WHETHER THERE ARE 100 BIRDS, OR 20 AVIARIES??
+  public boolean isFull();
+  
+  
 }
