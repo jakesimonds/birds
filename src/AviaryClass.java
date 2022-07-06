8fe3acf194de72d7cb332bbab28f46a483b08fb0
Nick Barnes <82965612+bicknarnes@users.noreply.github.com>
2022-07-06 12:05:45 -0400

    Update and rename aviaryClass.java to AviaryClass.java

---
+++ src/AviaryClass.java
@@ -0,0 +1,8 @@
+public class AviaryClass implements Aviary {
+
+  // Member Fields:
+  // numBirds, birdList, type (of birds stored in it)?? etc.
+  
+  // @Override methods from Aviary interface
+
+}
