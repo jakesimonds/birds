8a29687753628e9f45032ab895e8a43d84c2c2ba
Nick Barnes <82965612+bicknarnes@users.noreply.github.com>
2022-07-09 08:44:33 -0400

    Update Bird.java

--- src/Bird.java
+++ src/Bird.java
@@ -1,17 +1,20 @@
 public interface Bird {
     //interface for bird parent class
 
-
+    //return the number of wings that bird has
     public int getNumberOfWings();
 
+    //return the type of bird (ie. kiwi, moa, etc.)
     public String getBirdType();
 
+    // get a description/characteristic of that bird/family of birds
     public String getCharacteristic();
 
+    // return true if bird is extinct, false otherwise
     public boolean getExtinct();
 
-    //unsure about whether this is string or some kind of array?
-    public String getFoodPreference();
+    // return an array of FOOD types (enum class in its own file) (2-4)
+    public FOOD[] getFoodPreference();
 
 
 
