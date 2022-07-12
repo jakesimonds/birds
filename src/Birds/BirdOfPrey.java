ea266a4d3b79c2e69b40d0073249ff2cd27802c8
bicknarnes <ndavisbarnes@gmail.com>
2022-07-12 11:55:14 -0400

    Overloaded constructors for BirdClass & Children

--- src/Birds/BirdOfPrey.java
+++ src/Birds/BirdOfPrey.java
@@ -5,10 +5,37 @@ public class BirdOfPrey extends BirdClass {
 
     private String description;
 
-    // Constructor
+    //===================================== CONSTRUCTORS =========================================
+
     public BirdOfPrey(String name, int wingNum, boolean extinct, ArrayList<FOOD> foodPreference, String characteristic) {
         super(name, wingNum, extinct, foodPreference, characteristic);
         this.description = "Birds of prey all have sharp, hooked beaks with visible nostrils.";
         this.setBirdType("Bird of Prey");
     }
+
+
+    // if we can assume 2 wings:
+    public BirdOfPrey(String name, boolean extinct, ArrayList<FOOD> foodPreference, String characteristic) {
+        super(name, extinct, foodPreference, characteristic);
+        this.description = "Birds of prey all have sharp, hooked beaks with visible nostrils.";
+        this.setBirdType("Bird of Prey");
+    }
+
+
+    // if we can assume not extinct:
+    public BirdOfPrey(String name, int wingNum, ArrayList<FOOD> foodPreference, String characteristic) {
+        super(name, wingNum, foodPreference, characteristic);
+        this.description = "Birds of prey all have sharp, hooked beaks with visible nostrils.";
+        this.setBirdType("Bird of Prey");
+    }
+
+
+    // if we can assume both 2 wings and not extinct:
+    public BirdOfPrey(String name, ArrayList<FOOD> foodPreference, String characteristic) {
+        super(name, foodPreference, characteristic);
+        this.description = "Birds of prey all have sharp, hooked beaks with visible nostrils.";
+        this.setBirdType("Bird of Prey");
+    }
+
+
 }
--- src/Birds/FlightlessBird.java
+++ src/Birds/FlightlessBird.java
@@ -4,10 +4,40 @@ import java.util.ArrayList;
 public class FlightlessBird extends BirdClass {
   private String description;
 
+  //===================================== CONSTRUCTORS =========================================
+
   public FlightlessBird(String name, int wingNum, boolean extinct, ArrayList<FOOD> foodPreference, String characteristic) {
     super(name, wingNum, extinct, foodPreference, characteristic);
     this.description = "Flightless birds live on the ground and have no (or undeveloped) wings. " +
             "They include the emus, kiwis, and moas. Some (but not all) of these birds are extinct.";
     this.setBirdType("Flightless Bird");
   }
+
+
+  // if we can assume 2 wings:
+  public FlightlessBird(String name, boolean extinct, ArrayList<FOOD> foodPreference, String characteristic) {
+    super(name, extinct, foodPreference, characteristic);
+    this.description = "Flightless birds live on the ground and have no (or undeveloped) wings. " +
+            "They include the emus, kiwis, and moas. Some (but not all) of these birds are extinct.";
+    this.setBirdType("Flightless Bird");
+  }
+
+
+  // if we can assume not extinct:
+  public FlightlessBird(String name, int wingNum, ArrayList<FOOD> foodPreference, String characteristic) {
+    super(name, wingNum, foodPreference, characteristic);
+    this.description = "Flightless birds live on the ground and have no (or undeveloped) wings. " +
+            "They include the emus, kiwis, and moas. Some (but not all) of these birds are extinct.";
+    this.setBirdType("Flightless Bird");
+  }
+
+
+  // if we can assume both 2 wings and not extinct:
+  public FlightlessBird(String name, ArrayList<FOOD> foodPreference, String characteristic) {
+    super(name, foodPreference, characteristic);
+    this.description = "Flightless birds live on the ground and have no (or undeveloped) wings. " +
+            "They include the emus, kiwis, and moas. Some (but not all) of these birds are extinct.";
+    this.setBirdType("Flightless Bird");
+  }
+
 }
